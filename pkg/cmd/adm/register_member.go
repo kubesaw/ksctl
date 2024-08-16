4f614b1ce57d263f095477112fc4683f526085ae
Francisc Munteanu <fmuntean@redhat.com>
2024-08-16 11:15:13 +0200

    Update pkg/cmd/adm/register_member.go
    Co-authored-by: Matous Jobanek <mjobanek@redhat.com>

--- pkg/cmd/adm/register_member.go
+++ pkg/cmd/adm/register_member.go
@@ -139,7 +139,7 @@ func (v *registerMemberValidated) addCluster(ctx *extendedCommandContext, source
 		Namespace: sourceClusterDetails.namespace,
 	}
 	if err := waitForToolchainClusterSA(ctx.CommandContext, sourceClusterDetails.client, toolchainClusterSAKey, v.args.waitForReadyTimeout); err != nil {
-		ctx.Printlnf("The %s ServiceAccount in the %s cluster is not present.", toolchainClusterSAKey, sourceClusterType)
+		ctx.Printlnf("The %s ServiceAccount is not present in the %s cluster.", toolchainClusterSAKey, sourceClusterType)
 		ctx.Printlnf("Please check the %s ToolchainCluster ServiceAccount in the %s %s cluster.", toolchainClusterSAKey, sourceClusterDetails.apiEndpoint, sourceClusterType)
 		return err
 	}
