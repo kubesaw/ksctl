7719c2a2450c1daf0f3bd711d57ba63959c8e937
Devtools <devtools@redhat.com>
2024-08-27 10:54:54 +0200

    improve comment

--- pkg/assets/sandbox_config.go
+++ pkg/assets/sandbox_config.go
@@ -14,7 +14,9 @@ type Clusters struct {
 type MemberCluster struct {
 	Name          string `yaml:"name"`
 	ClusterConfig `yaml:",inline"`
-	// SeparateKustomizeComponent when set to true, then the manifests for the member cluster will be generated in a separate Kustomize folder
+	// SeparateKustomizeComponent when set to true, then the manifests for the member cluster will be generated in a separate
+	// Kustomize component (a directory structure that will contain all the generated manifests including the kustomization.yaml files).
+	// The name of the root folder will have the same name as the name of the member cluster.
 	SeparateKustomizeComponent bool `yaml:"separateKustomizeComponent"`
 }
 
