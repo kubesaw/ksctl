3f3bb7036c562eb66c902e8461e56c6261729ef7
Matous Jobanek <mjobanek@redhat.com>
2025-01-10 18:41:19 +0100

    fix: unregister-member deletes secret & add needed permissions (#99)

--- pkg/test/toolchaincluster.go
+++ pkg/test/toolchaincluster.go
@@ -6,6 +6,7 @@ import (
 
 	toolchainv1alpha1 "github.com/codeready-toolchain/api/api/v1alpha1"
 	"github.com/codeready-toolchain/toolchain-common/pkg/test"
+	v1 "k8s.io/api/core/v1"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -18,6 +19,11 @@ func NewToolchainCluster(modifiers ...ToolchainClusterModifier) *toolchainv1alph
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      "member1",
 			Namespace: test.HostOperatorNs,
+		},
+		Spec: toolchainv1alpha1.ToolchainClusterSpec{
+			SecretRef: toolchainv1alpha1.LocalSecretReference{
+				Name: "member1-secret",
+			},
 		},
 		Status: toolchainv1alpha1.ToolchainClusterStatus{
 			APIEndpoint: "https://api.member.com:6443",
@@ -33,6 +39,9 @@ func AssertToolchainClusterDoesNotExist(t *testing.T, fakeClient *test.FakeClien
 	deletedCluster := &toolchainv1alpha1.ToolchainCluster{}
 	err := fakeClient.Get(context.TODO(), test.NamespacedName(toolchainCluster.Namespace, toolchainCluster.Name), deletedCluster)
 	require.True(t, apierrors.IsNotFound(err), "the ToolchainCluster should be deleted")
+
+	err = fakeClient.Get(context.TODO(), test.NamespacedName(toolchainCluster.Namespace, toolchainCluster.Spec.SecretRef.Name), &v1.Secret{})
+	require.True(t, apierrors.IsNotFound(err), "the ToolchainCluster secret should be deleted")
 }
 
 func AssertToolchainClusterSpec(t *testing.T, fakeClient *test.FakeClient, expectedToolchainCluster *toolchainv1alpha1.ToolchainCluster) {
@@ -47,5 +56,6 @@ type ToolchainClusterModifier func(toolchainCluster *toolchainv1alpha1.Toolchain
 func ToolchainClusterName(name string) ToolchainClusterModifier {
 	return func(toolchainCluster *toolchainv1alpha1.ToolchainCluster) {
 		toolchainCluster.Name = name
+		toolchainCluster.Spec.SecretRef.Name = name + "-secret"
 	}
 }
