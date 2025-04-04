94e6d785a1c8040cd0ca16ae75e2ec611c3a6693
Lukas Krejci <lkrejci@redhat.com>
2025-04-04 14:40:30 +0200

    implement the unban command

---
+++ pkg/cmd/unban.go
@@ -0,0 +1,83 @@
+package cmd
+
+import (
+	"fmt"
+
+	toolchainv1alpha1 "github.com/codeready-toolchain/api/api/v1alpha1"
+	"github.com/codeready-toolchain/toolchain-common/pkg/hash"
+	"github.com/kubesaw/ksctl/pkg/client"
+	"github.com/kubesaw/ksctl/pkg/configuration"
+	clicontext "github.com/kubesaw/ksctl/pkg/context"
+	"github.com/kubesaw/ksctl/pkg/ioutils"
+	"github.com/spf13/cobra"
+	runtimeclient "sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+func NewUnbanCommand() *cobra.Command {
+	return &cobra.Command{
+		Use:   "unban <email>",
+		Short: "Unban the user so that they can use the platform again",
+		Long:  "Unban the user that previously registered with the provided email so that they can start using the platform again.",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			term := ioutils.NewTerminal(cmd.InOrStdin, cmd.OutOrStdout)
+			ctx := clicontext.NewCommandContext(term, client.DefaultNewClient)
+			return Unban(ctx, args[0])
+		},
+	}
+}
+
+func Unban(ctx *clicontext.CommandContext, email string) error {
+	cl, cfg, err := getClient(ctx)
+	if err != nil {
+		return err
+	}
+
+	emailHash := hash.EncodeString(email)
+	list := &toolchainv1alpha1.BannedUserList{}
+
+	if err = cl.List(ctx.Context, list,
+		runtimeclient.InNamespace(cfg.OperatorNamespace),
+		runtimeclient.MatchingLabels{toolchainv1alpha1.BannedUserEmailHashLabelKey: emailHash}); err != nil {
+		return err
+	}
+
+	if len(list.Items) == 0 {
+		ctx.Println("No BannedUser objects found with given email.")
+		return nil
+	}
+
+	if len(list.Items) > 1 {
+		ctx.Println("More than 1 BannedUser found for given email. Found:")
+		for _, bu := range list.Items {
+			ctx.PrintObject(&bu, "")
+		}
+		return fmt.Errorf("expected 0 or 1 BannedUser objects to correspond to the email '%s' but %d found", email, len(list.Items))
+	}
+
+	bu := &list.Items[0]
+	if bu.Spec.Email != email {
+		ctx.PrintObject(bu, "Inconsistent BannedUser encountered - the email doesn't correspond to the email-hash")
+		return fmt.Errorf("inconsistent BannedUser, the email '%s' doesn't correspond to the email-hash label value '%s'", bu.Spec.Email, emailHash)
+	}
+
+	err = cl.Delete(ctx.Context, bu)
+	if err != nil {
+		return err
+	}
+
+	ctx.Println("User successfully unbanned")
+	return nil
+}
+
+func getClient(ctx *clicontext.CommandContext) (runtimeclient.Client, *configuration.ClusterConfig, error) {
+	cfg, err := configuration.LoadClusterConfig(ctx, configuration.HostName)
+	if err != nil {
+		return nil, nil, err
+	}
+	cl, err := ctx.NewClient(cfg.Token, cfg.ServerAPI)
+	if err != nil {
+		return nil, nil, err
+	}
+	return cl, &cfg, nil
+}
---
+++ pkg/cmd/unban_test.go
@@ -0,0 +1,138 @@
+package cmd_test
+
+import (
+	"testing"
+
+	toolchainv1alpha1 "github.com/codeready-toolchain/api/api/v1alpha1"
+	"github.com/codeready-toolchain/toolchain-common/pkg/hash"
+	"github.com/kubesaw/ksctl/pkg/cmd"
+	"github.com/kubesaw/ksctl/pkg/configuration"
+	clicontext "github.com/kubesaw/ksctl/pkg/context"
+	"github.com/kubesaw/ksctl/pkg/ioutils"
+	. "github.com/kubesaw/ksctl/pkg/test"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func TestUnbanCommand(t *testing.T) {
+	t.Run("fails with no parameters", func(t *testing.T) {
+		cmd := cmd.NewUnbanCommand()
+		_, err := cmd.ExecuteC()
+		require.Error(t, err)
+		require.Contains(t, err.Error(), "accepts 1 arg(s), received 0")
+	})
+	t.Run("fails with more than 1 parameter", func(t *testing.T) {
+		cmd := cmd.NewUnbanCommand()
+		cmd.SetArgs([]string{"a", "b"})
+		_, err := cmd.ExecuteC()
+		require.Error(t, err)
+		require.Contains(t, err.Error(), "accepts 1 arg(s), received 2")
+	})
+	t.Run("runs with exactly 1 parameter", func(t *testing.T) {
+		cmd := cmd.NewUnbanCommand()
+		cmd.SetArgs([]string{"me@home"})
+		_, err := cmd.ExecuteC()
+		require.Error(t, err)
+		// this means we started executing the command (and are not able to find the configuration file)
+		// but that's enough for us to know that we got past the command validation.
+		require.Contains(t, err.Error(), "unable to read the file")
+	})
+}
+
+func TestUnbanWhenNoneExists(t *testing.T) {
+	// given
+	SetFileConfig(t, Host())
+	term := NewFakeTerminal()
+	newClient, _ := NewFakeClients(t)
+	ctx := clicontext.NewCommandContext(term, newClient)
+
+	// when
+	err := cmd.Unban(ctx, "me@home")
+
+	// then
+	require.NoError(t, err)
+	output := term.Output()
+	assert.NotContains(t, output, "User successfully unbanned")
+	assert.Contains(t, output, "No BannedUser objects found with given email.")
+}
+
+func TestUnbanWhenMoreThanOneExists(t *testing.T) {
+	// given
+	SetFileConfig(t, Host())
+	term := NewFakeTerminal()
+	bannedUser1 := newBannedUser(t, "me@home", false, term)
+	bannedUser2 := newBannedUser(t, "me@home", false, term)
+	bannedUser2.Name = "bannedUser2"
+	newClient, _ := NewFakeClients(t, bannedUser1, bannedUser2)
+	ctx := clicontext.NewCommandContext(term, newClient)
+
+	// when
+	err := cmd.Unban(ctx, "me@home")
+
+	// then
+	require.Error(t, err)
+	output := term.Output()
+	assert.NotContains(t, output, "User successfully unbanned")
+	assert.Contains(t, output, "More than 1 BannedUser found for given email. Found:")
+}
+
+func TestUnbanWithInconsistentBannedUser(t *testing.T) {
+	// given
+	SetFileConfig(t, Host())
+	term := NewFakeTerminal()
+	bannedUser := newBannedUser(t, "me@home", true, term)
+	newClient, _ := NewFakeClients(t, bannedUser)
+	ctx := clicontext.NewCommandContext(term, newClient)
+
+	// when
+	err := cmd.Unban(ctx, "me@home")
+
+	// then
+	require.Error(t, err)
+	output := term.Output()
+	assert.Contains(t, output, "Inconsistent BannedUser encountered - the email doesn't correspond to the email-hash")
+}
+
+func TestUnban(t *testing.T) {
+	// given
+	SetFileConfig(t, Host())
+	term := NewFakeTerminal()
+	bannedUser := newBannedUser(t, "me@home", false, term)
+	newClient, _ := NewFakeClients(t, bannedUser)
+	ctx := clicontext.NewCommandContext(term, newClient)
+
+	// when
+	err := cmd.Unban(ctx, "me@home")
+
+	// then
+	require.NoError(t, err)
+	output := term.Output()
+	assert.Contains(t, output, "User successfully unbanned")
+}
+
+func newBannedUser(t *testing.T, email string, inconsistent bool, term ioutils.Terminal) *toolchainv1alpha1.BannedUser {
+	var emailToUse string
+	if inconsistent {
+		emailToUse = email + ".not"
+	} else {
+		emailToUse = email
+	}
+
+	cfg, err := configuration.LoadClusterConfig(term, configuration.HostName)
+	require.NoError(t, err)
+
+	return &toolchainv1alpha1.BannedUser{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "asdf",
+			Namespace: cfg.OperatorNamespace,
+			Labels: map[string]string{
+				toolchainv1alpha1.BannedUserEmailHashLabelKey: hash.EncodeString(email),
+			},
+		},
+		Spec: toolchainv1alpha1.BannedUserSpec{
+			Email:  emailToUse,
+			Reason: "laughs and giggles",
+		},
+	}
+}
