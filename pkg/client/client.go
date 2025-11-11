8c3b0fb1818e9394c98ae125cb12af13fa2d710e
Lukas Krejci <metlos@users.noreply.github.com>
2025-11-11 13:32:44 +0100

    remove unused code from pkg/client/client (#134)

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -5,17 +5,13 @@ import (
 	"crypto/tls"
 	"fmt"
 	"net/http"
-	"reflect"
 	"time"
 
 	toolchainv1alpha1 "github.com/codeready-toolchain/api/api/v1alpha1"
-	commonclient "github.com/codeready-toolchain/toolchain-common/pkg/client"
 	"github.com/kubesaw/ksctl/pkg/configuration"
 	clicontext "github.com/kubesaw/ksctl/pkg/context"
-	"github.com/kubesaw/ksctl/pkg/ioutils"
 	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
 
-	"github.com/ghodss/yaml"
 	configv1 "github.com/openshift/api/config/v1"
 	projectv1 "github.com/openshift/api/project/v1"
 	routev1 "github.com/openshift/api/route/v1"
@@ -23,14 +19,10 @@ import (
 	userv1 "github.com/openshift/api/user/v1"
 	olmv1 "github.com/operator-framework/api/pkg/operators/v1"
 	olmv1alpha1 "github.com/operator-framework/api/pkg/operators/v1alpha1"
-	errs "github.com/pkg/errors"
 	corev1 "k8s.io/api/core/v1"
 	rbacv1 "k8s.io/api/rbac/v1"
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
-	"k8s.io/apimachinery/pkg/api/meta"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
-	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
@@ -96,18 +88,18 @@ func NewKubeClientFromKubeConfig(kubeConfigPath string) (cl runtimeclient.Client
 
 	kubeConfig, err = clientcmd.LoadFromFile(kubeConfigPath)
 	if err != nil {
-		return
+		return cl, err
 	}
 	clientConfig, err = clientcmd.NewDefaultClientConfig(*kubeConfig, nil).ClientConfig()
 	if err != nil {
-		return
+		return cl, err
 	}
 	cl, err = NewClientFromRestConfig(clientConfig)
 	if err != nil {
-		return
+		return cl, err
 	}
 
-	return
+	return cl, err
 }
 
 func newTlsVerifySkippingTransport() http.RoundTripper {
@@ -278,115 +270,3 @@ func GetUserTier(cfg configuration.ClusterConfig, cl runtimeclient.Client, name 
 	}
 	return obj, nil
 }
-
-// Ensure creates or updates the given object and returns if the object was either created or updated (which means
-// that no error occurred and the administrator confirmed execution of the action)
-func Ensure(term ioutils.Terminal, cl runtimeclient.Client, obj runtimeclient.Object) (bool, error) {
-	namespacedName := types.NamespacedName{
-		Namespace: obj.GetNamespace(),
-		Name:      obj.GetName(),
-	}
-	return ensure(term, cl, namespacedName, obj)
-}
-
-func ensure(term ioutils.Terminal, cl runtimeclient.Client, namespacedName types.NamespacedName, obj runtimeclient.Object) (bool, error) {
-	content, err := yaml.Marshal(obj)
-	if err != nil {
-		return false, err
-	}
-	resourceKind := obj.GetObjectKind().GroupVersionKind().Kind
-	// if GVK is not defined in the given object, then let's use its type
-	if resourceKind == "" {
-		resourceKind = reflect.TypeOf(obj).Elem().Name()
-	}
-	term.PrintContextSeparatorWithBodyf(string(content), "Using %s resource:", resourceKind)
-
-	existing := obj.DeepCopyObject().(runtimeclient.Object)
-	if err := cl.Get(context.TODO(), namespacedName, existing); err != nil && !apierrors.IsNotFound(err) {
-		return false, err
-	} else if err == nil {
-		term.Printlnf("There is an already existing %s with the same name: %s", resourceKind, namespacedName)
-		if !term.AskForConfirmation(ioutils.WithDangerZoneMessagef(
-			"update of the already created "+resourceKind, "update the %s with the hard-coded version?", resourceKind)) {
-			return false, nil
-		}
-		metaNew, err := meta.Accessor(obj)
-		if err != nil {
-			return false, errs.Wrapf(err, "cannot get metadata from %+v", obj)
-		}
-		metaExisting, err := meta.Accessor(existing)
-		if err != nil {
-			return false, errs.Wrapf(err, "cannot get metadata from %+v", existing)
-		}
-		metaNew.SetResourceVersion(metaExisting.GetResourceVersion())
-		// make sure that when updating a 'service' object, we retain its existing the `spec.clusterIP` field,
-		// otherwise we get the following error: `Service "prometheus" is invalid: spec.clusterIP: Invalid value: "": field is immutable`
-		if err := commonclient.RetainClusterIP(obj, existing); err != nil {
-			return false, err
-		}
-
-		if err := cl.Update(context.TODO(), obj); err != nil {
-			return false, err
-		}
-		term.Printlnf("\nThe '%s' %s has been updated", namespacedName.Name, resourceKind)
-		return true, nil
-	}
-
-	if !term.AskForConfirmation(ioutils.WithMessagef("create the %s resource with the name %s ?", resourceKind, namespacedName)) {
-		return false, nil
-	}
-	if err := cl.Create(context.TODO(), obj); err != nil {
-		return false, err
-	}
-	term.Printlnf("\nThe '%s' %s has been created", namespacedName, resourceKind)
-	return true, nil
-}
-
-// Create creates the resource only if it does not exist yet (ie, if a resource of the same kind in the same namespace/name doesn't exist yet)
-func Create(term ioutils.Terminal, cl runtimeclient.Client, obj runtimeclient.Object) error {
-	namespacedName := types.NamespacedName{
-		Namespace: obj.GetNamespace(),
-		Name:      obj.GetName(),
-	}
-	objCopy := obj.DeepCopyObject().(runtimeclient.Object)
-	if err := cl.Get(context.TODO(), namespacedName, objCopy); err != nil && !apierrors.IsNotFound(err) {
-		return err
-	} else if apierrors.IsNotFound(err) {
-		if err := cl.Create(context.TODO(), obj); err != nil {
-			return err
-		}
-		term.Printlnf("\nThe '%s' %s has been created", namespacedName, reflect.TypeOf(obj).Elem().Name())
-		return nil
-	}
-	term.Printlnf("\nThe '%s' %s already exists", namespacedName, reflect.TypeOf(obj).Elem().Name())
-	return nil
-}
-
-// GetRouteURL return the scheme+host of the route with the given namespaced name.
-// Since routes may take a bit of time to be available, this func uses a wait loop
-// to make sure that the route was created, or fails after a timeout.
-func GetRouteURL(term ioutils.Terminal, cl runtimeclient.Client, namespacedName types.NamespacedName) (string, error) {
-	term.Printlnf("Waiting for '%s' route to be available...", namespacedName.Name)
-	route := routev1.Route{}
-	if err := wait.PollUntilContextTimeout(context.TODO(), retryInterval, timeout, false, func(ctx context.Context) (done bool, err error) {
-		if err := cl.Get(context.TODO(), namespacedName, &route); err != nil && !apierrors.IsNotFound(err) {
-			return false, err
-		}
-		if len(route.Status.Ingress) == 0 {
-			return false, nil // route is not ready yet
-		}
-		return true, nil
-	}); err != nil {
-		return "", errs.Wrapf(err, "unable to get route to %s", namespacedName)
-	}
-	scheme := "https"
-	if route.Spec.TLS == nil || *route.Spec.TLS == (routev1.TLSConfig{}) {
-		scheme = "http"
-	}
-	return fmt.Sprintf("%s://%s/%s", scheme, route.Spec.Host, route.Spec.Path), nil
-}
-
-var (
-	timeout       = 5 * time.Second
-	retryInterval = 200 * time.Millisecond
-)
